f4f30d82222137491204f7e3d6288ef3fd1cd2fb
Christoph Schlumpf <christoph.schlumpf@sadayamanda.org>
2022-11-08 16:31:14 +0100

    first iteration

--- server.mjs
+++ server.mjs
@@ -1,8 +1,27 @@
 import { createServer } from 'http';
+import { co2 } from '@tgwf/co2';
+import axios from 'axios';
+const co2Emission = new co2();
+const greenHost = true // Is the data transferred from a green host?
 
 console.log("Loading...");
 
+var data
+
+axios.get('https://api.websitecarbon.com/site?url=https://green-to-go.vercel.app')
+  .then(function (response) {
+    console.log(response.data);
+    console.log(response.data.statistics.co2.grid);
+    console.log(response.data.statistics.co2.renewable);
+    const estimatedCO2 = co2Emission.perByte(response.data.bytes, false)
+    data = response.data.statistics.co2.grid
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
 createServer((req, res) => {
-  res.write('Hello Green to Go!');
+  res.write(JSON.stringify(data));
   res.end();
-}).listen(process.env.PORT);
+}).listen(process.env.PORT || 8080);
